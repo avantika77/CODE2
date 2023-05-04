810e1f4053417cf54481c6bb6a940146ddf5e216
Avantika Shrivastava <103310867+avantika77@users.noreply.github.com>
2023-05-04 14:39:39 +0530

    Add files via upload

---
+++ basic of java.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class  Main
+{ public static void main(String args[])
+    {
+    Scanner sc =new Scanner (System.in);
+	String name =sc.nextLine();
+		System.out.println(name);
+    System.out .println("enter value of a:");
+	int a=sc.nextInt();
+	System.out .println("enter value of b:");
+	int b=sc.nextInt();
+	System.out .println("Sum of a and b:");
+	int sum =a+b;
+	System.out.println(sum);
+	}
+}
